092275a8ce8cdb5a0ce383b5aeccadefe868899e
magdalena <magdapolak96@gmail.com>
2020-07-09 21:40:09 +0200

    fetch the data with proper handling

--- src/users/Users.tsx
+++ src/users/Users.tsx
@@ -1,31 +1,42 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import UserRow from './UserRow';
 import IUser from './types/IUser'
 import UsersSearch from './SearchUsers';
 import { connect } from 'react-redux';
-import { IUsersState } from "./store/types"
+import { IUsersState, IGetUsersAction } from "./store/types"
+import { fetchUsers } from './store/actions';
+import { ThunkDispatch } from 'redux-thunk';
 
-const Users: React.FC<{ users: IUser[] }> = ({ users }) => {
+const Users: React.FC<{ users: IUser[], loadingUsersError: string, loadingUsers: boolean, dispatch: ThunkDispatch<{}, {}, IGetUsersAction> }> 
+  = ({ users, loadingUsers, loadingUsersError, dispatch }) => {
 
   const [searchPhrase, setSearchPhrase] = useState("")
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
 
   const filteredUsers = useMemo(() => {
-    return users.filter(user => searchPhrase && user.name.toLowerCase().includes(searchPhrase.toLowerCase()))
+    return users.filter(user => !searchPhrase || user.name.toLowerCase().includes(searchPhrase.toLowerCase()))
   }, [users, searchPhrase])
 
   return <div className="users-container">
       <h1 className="users-header">Users list</h1>
-      <UsersSearch onSearch={text => setSearchPhrase(text)}/>
-      <div className="users">
-        {filteredUsers.map((user: IUser) => <UserRow key={user.id} id={user.id} name={user.name} username={user.username}/>)}
-      </div>
+      {loadingUsers ? <p>Loading...</p> : 
+      <>
+        <UsersSearch onSearch={text => setSearchPhrase(text)}/>
+        <div className="users">
+          {filteredUsers.map((user: IUser) => <UserRow key={user.id} id={user.id} name={user.name} username={user.username}/>)}
+        </div>
+      </>
+    }
+    {loadingUsersError && <p className="error">{loadingUsersError}</p>}
   </div>
 }
 
 const mapStateToProps = (state: IUsersState) => ({
   users: state.users,
   loadingUsers: state.loading,
-  error: state.error
+  loadingUsersError: state.error
 })
 
 export default connect(mapStateToProps)(Users);
